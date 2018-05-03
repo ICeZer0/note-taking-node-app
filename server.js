768dfd1f91b7d0f2ebad942ca5eb614fb8de0fd2
Isaac <ice.durant7@outlook.com>
2018-05-03 00:11:39 -0500

    CRUD

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import {express} from 'express';
-import {bodyParser} from 'body-parser';
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
@@ -7,11 +7,30 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+//DB configuration
+import dbConfig from './config/database.config';
+import mongoose from 'mongoose';
+
+mongoose.Promise = global.Promise;
+
+//conenction to DB
+mongoose.connect(dbConfig.url)
+.then(() => { 
+    console.log("Connected to database...");
+}).catch(err => {
+    console.log(err)
+    console.log("Could not connect to database. Exiting now...");
+    process.exit();
+})
+
 app.get('/',(request, response) => { 
     response.json({"message": "Welcome to NoteTaker. Easily organize and store your notes"});
 });
 
-app.list(3000, () => {
+//require notes routes
+require('./app/routes/note.routes')(app);
+
+app.listen(3000, () => {
     console.log("Server is listening on port 3000")
 });
 
